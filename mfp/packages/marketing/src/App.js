6214cb39a202c8a6030fa4a3824bb8d6a1cfe59c
kemet price <Kemet.Price1@gmail.com>
2023-02-01 12:03:52 -0500

    Section5: Generic Ties Between Projects

--- mfp/packages/marketing/src/App.js
+++ mfp/packages/marketing/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { StylesProvider } from '@material-ui/core/styles';
 
@@ -6,14 +6,16 @@ import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
 export default () => {
-    return <div>
-        <StylesProvider>
-            <BrowserRouter>
-            <Switch>
-                <Route exact path="/pricing" component={Pricing} />
-                <Route path="/" component={Landing} />
-            </Switch>
-            </BrowserRouter>
-        </StylesProvider>
+  return (
+    <div>
+      <StylesProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
     </div>
-}
+  );
+};
